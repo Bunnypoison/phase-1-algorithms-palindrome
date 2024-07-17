ee6e48759e049ed44227290cc7092440bec18be7
Erica Domerese <xrabbitonthemoonx@gmail.com>
2024-07-17 11:53:37 -0500

    algorithms-palindrome-lab

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const cleanWord = word.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  return cleanWord === cleanWord.split('').reverse().join('');
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
+// Export the isPalindrome function
 module.exports = isPalindrome;
+
+// Test the function
+console.log("Expecting: true");
+console.log("=>", isPalindrome("racecar"));
+
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
+
+// Additional test cases
+console.log("Expecting: true");
+console.log("=>", isPalindrome("abba"));
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("a"));
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("ab"));
